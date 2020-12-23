e4c75653e7f9c809b7b227edac8e13f5efc24808
GatsAleks <gatsenko.aleksandra@gmail.com>
2020-12-23 09:22:02 +0000

    Carousel

---
+++ js/carousel.js
@@ -0,0 +1,46 @@
+{
+    const images = [
+        'images/menu/cezar.jpg',
+        'images/menu/chicken.jpg',
+        'images/menu/meat.jpg',
+        'images/menu/pancake.jpg',
+        'images/menu/salmon.jpg',
+        'images/menu/steak.jpg',
+        'images/menu/vareniki.jpg',
+        'images/menu/vareniki-mashroom.jpg'
+    ]
+
+    let currentIdx = 0;
+
+    function showCurrent() {
+        const imgElement1 = document.querySelector('.carousel-trio .img1');
+        const imgElement2 = document.querySelector('.carousel-trio .img2');
+        const imgElement3 = document.querySelector('.carousel-trio .img3');
+        const imgElement4 = document.querySelector('.carousel-trio .img4');
+        const idx2 = currentIdx + 1 >= images.length ? 0 : currentIdx + 1;
+        const idx3 = idx2 + 1 >= images.length ? 0 : idx2 + 1;
+        const idx4 = idx3 + 1 >= images.length ? 0 : idx3 + 1;
+        // console.log(currentIdx, idx2, idx3, idx4);
+        imgElement1.src = images[currentIdx];
+        imgElement2.src = images[idx2];
+        imgElement3.src = images[idx3];
+        imgElement4.src = images[idx4];
+    }
+
+    function showNext() {
+        currentIdx++;
+        if (currentIdx >= images.length) currentIdx = 0;
+        showCurrent();
+    }
+
+    function showPrev() {
+        currentIdx--;
+        if (currentIdx < 0) currentIdx = images.length - 1;
+        showCurrent();
+    }
+
+    setInterval(showNext, 3000);
+    document.querySelector('.carousel-trio .next').addEventListener('click', showNext);
+    document.querySelector('.carousel-trio .prev').addEventListener('click', showPrev);
+    showCurrent();
+}
